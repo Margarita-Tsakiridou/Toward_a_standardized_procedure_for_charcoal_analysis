a4c5f13d212eaa17110dc97e0f00bc97dd30e357
Margie <m_tsakiridou@hotmail.gr>
2022-01-25 21:14:04 +0000

    First try

---
+++ scripts/Boxplotting_Script.R
@@ -0,0 +1,36 @@
+setwd("G:/My Drive/3. Paper no. 2 - Chemicals__send2LC/3. Fresh Charcoal Experiment/3. Figures/new_hypervisualized/Rwork")
+data <- read.csv(file= "data.csv", header=TRUE, sep = ",")
+Treatment <- data$Treatment
+l<-split(data, Treatment)
+
+
+H2O12h <- l[[1]]
+H2O6h <- l[[2]]
+H2O233pc <- l[[3]]
+H2O28pc <- l[[4]]
+HNO3 <- l[[5]]
+KOH <- l[[6]]
+Na6PO18 <- l[[7]]
+NaCLO12.5 <- l[[8]]
+NACLO2.5pc <- l[[9]]
+
+H2O12h <- H2O12h[,4:8]               ###subsetting info from the lists because that's only how I know
+H2O6h <- H2O6h[,4:8]
+H2O233pc <- H2O233pc[,4:8]
+H2O28pc <-H2O28pc[,4:8]
+HNO3 <- HNO3[,4:8]
+KOH <- KOH[,4:8]
+Na6PO18 <- Na6PO18[,4:8]
+NaCLO12.5 <- NaCLO12.5[,4:8]
+NACLO2.5pc <- NACLO2.5pc[,4:8]
+
+
+bxp_H2O12h <- boxplot(H2O12h, ylim=c(-3,5))
+bxp_H2O6h <- boxplot(H2O6h, ylim=c(-3,5))
+bxp_H2O233pc <- boxplot(H2O233pc, ylim=c(-3,5))
+bxp_H2O28pc <- boxplot(H2O28pc, ylim=c(-3,5))
+bxp_HNO3 <- boxplot(HNO3, ylim=c(-3,5))
+bxp_KOH <- boxplot(KOH, ylim=c(-3,5))
+bxp_Na6PO18 <- boxplot(Na6PO18, ylim=c(-3,5))
+bxp_NaCLO12.5 <- boxplot(NaCLO12.5, ylim=c(-3,5))
+bxp_NACLO2.5pc <- boxplot(NACLO2.5pc, ylim=c(-3,5))
